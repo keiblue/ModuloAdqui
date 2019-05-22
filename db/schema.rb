567ba69921adbbab0a1f28a1f52ea9fdb6f709d4
Felipe Malermo <keibook@Felipes-MacBook-Pro.local>
2019-05-21 23:16:26 -0400

    :3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_234311) do
+ActiveRecord::Schema.define(version: 2019_05_22_021656) do
+
+  create_table "roles", options: "ENGINE=MyISAM DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "role_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=MyISAM DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
