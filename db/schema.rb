fb9c0542cb71608a391d70b8f7c464206cfa1669
Felipe Malermo <keibook@Felipes-MacBook-Pro.local>
2019-05-22 02:00:10 -0400

    diseño login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_021656) do
+ActiveRecord::Schema.define(version: 2019_05_22_032431) do
 
   create_table "roles", options: "ENGINE=MyISAM DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "role_name"
@@ -29,8 +29,10 @@ ActiveRecord::Schema.define(version: 2019_05_22_021656) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "role_id", default: 3
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
 end
