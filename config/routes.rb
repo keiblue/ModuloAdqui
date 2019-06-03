bc95fa22f2f592df0de24547dcc59aaa2c213b99
Felipe Malermo <f.malermo@gmail.com>
2019-06-03 19:05:47 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   resources :orders
   resources :roles
   #resources :accounts
-  devise_for :users
+  devise_for :users, :skip => [:registrations] 
+   as :user do
+     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+     put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
