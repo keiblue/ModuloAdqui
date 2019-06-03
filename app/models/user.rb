a72e2f083575202578db5a3435e5b5e8ee07cad5
Felipe Malermo <f.malermo@gmail.com>
2019-06-03 19:01:39 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   validates_uniqueness_of :rutito
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
+  if devise :registerable
+    redirect_to root_path
+  end
 end
