e7ce52044ec35e0a7a6b1938f6c38c8f5c81a361
Felipe Malermo <f.malermo@gmail.com>
2019-06-03 19:53:29 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,:registerable,
          :recoverable, :rememberable, :validatable
- 
+ after_destroy :update_orders
+  
+  def update_orders
+    orders = Order.where(user_id: self.id)
+    orders.each do |order|
+      order.user.id = 1
+      order.save
+    end
+  end
 end
