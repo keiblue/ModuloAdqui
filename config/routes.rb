d169edd2c0c977593a75e951dfcd7d701468652f
Felipe Malermo <f.malermo@gmail.com>
2019-06-03 19:39:14 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -29,10 +29,10 @@ Rails.application.routes.draw do
   patch 'accounts/:id', action: :update, controller: 'accounts'
   delete 'accounts/:id', action: :destroy, controller: 'accounts'
 
-  get 'provider/items/:provider_id', action: :index, controller: 'items', as:'items_provider'
-  get 'provider/items/new/:provider_id', action: :new, controller: 'items', as:'new_item_provider'
-  get 'provider/items/:id/:provider_id', action: :show, controller: 'items', as:'item_provider'
-  get 'provider/items/:id/edit/:provider_id', action: :edit, controller: 'items', as:'edit_item_provider'
-  delete 'provider/items/:id/:provider_id', action: :destroy, controller: 'items', as: 'delete_item_provider'
+  get 'provider/:provider_id/items/', action: :index, controller: 'items', as:'items_provider'
+  get 'provider/:provider_id/items/new/', action: :new, controller: 'items', as:'new_item_provider'
+  get 'provider/:provider_id/items/:id/', action: :show, controller: 'items', as:'item_provider'
+  get 'provider/:provider_id/items/:id/edit/', action: :edit, controller: 'items', as:'edit_item_provider'
+  delete 'provider/:provider_id/items/:id/', action: :destroy, controller: 'items', as: 'delete_item_provider'
 
 end
