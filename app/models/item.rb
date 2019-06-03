9b2df03058e72df6d9c3d03a3b8fc0e20b5d60d8
Felipe Malermo <f.malermo@gmail.com>
2019-06-03 18:48:08 -0400

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
     has_many :details
     has_many :orders , through: :details
     
-    before_destroy: :delete_detail
+    before_destroy :delete_detail
     
     def delete_detail
         detail = Detail.where(item_id: self.id)
