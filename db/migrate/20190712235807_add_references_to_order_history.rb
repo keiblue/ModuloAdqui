ba1044a0654a6fc9278e27dbd8bd1d52f635b665
Felipe Malermo <f.malermo@gmail.com>
2019-07-13 02:12:44 -0400

    Update 20190712235807_add_references_to_order_history.rb

--- db/migrate/20190712235807_add_references_to_order_history.rb
+++ db/migrate/20190712235807_add_references_to_order_history.rb
@@ -5,8 +5,8 @@ class AddReferencesToOrderHistory < ActiveRecord::Migration[5.2]
     remove_reference :order_histories, :status, foreign_key: false , default: 1
   end
   def up
-    add_reference :order_histories, :user, foreign_key: false , default: 1
-    add_reference :order_histories, :order, foreign_key: false , default: 1
-    add_reference :order_histories, :status, foreign_key: false , default: 1
+    add_reference :order_histories, :user, foreign_key: true , default: 1
+    add_reference :order_histories, :order, foreign_key: true , default: 1
+    add_reference :order_histories, :status, foreign_key: true , default: 1
   end
 end
